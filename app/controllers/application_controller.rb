183d7ce269437ce37af96c14202ab63aa4e4ece3
Mwanzia24 <mwanzaitom19@gmail.com>
2023-06-06 07:05:56 -0400

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,39 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Welcome to the Drinks Tracker API!" }.to_json
+  end
+  
+  # Example route for retrieving all drinks
+  get "/drinks" do
+    drinks = Drink.all
+    drinks.to_json
+  end
+
+  # Example route for retrieving a specific drink
+  get "/drinks/:id" do
+    drink = Drink.find(params[:id])
+    drink.to_json
+  end
+  
+  # Example route for creating a new drink
+  post "/drinks" do
+    drink = Drink.create(params)
+    drink.to_json
+  end
+  
+  # Example route for updating a drink
+  put "/drinks/:id" do
+    drink = Drink.find(params[:id])
+    drink.update(params)
+    drink.to_json
+  end
+  
+  # Example route for deleting a drink
+  delete "/drinks/:id" do
+    drink = Drink.find(params[:id])
+    drink.destroy
+    { message: "Drink deleted successfully" }.to_json
   end
 
 end
